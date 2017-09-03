aba6b9f5df81d53980ed48d14e09ddedbce8ecc3
Kyle Belfort <kyle@Kyles-MacBook-Air.local>
2017-09-02 18:25:45 -0700

    Words for cards are now randomized

--- Game.java
+++ Game.java
@@ -1,6 +1,9 @@
 import java.util.Random;
 import java.util.Scanner;
 import java.io.*;
+import java.util.ArrayList; 
+import java.util.*;
+
 
 
 public class Game
@@ -37,15 +40,52 @@ public class Game
    
    {
       cards = new Card[25];
-      File wordFile = new File("Dictionary.txt");
-      Scanner inputFile = new Scanner(wordFile);
       
+      
+      //get number of words in Dict file and add words to dict ArrayList
+      File wordFile = new File("Dictionary.txt");
+      Scanner inputFile1 = new Scanner(wordFile);        
+      int wordCount = 0;   
+      ArrayList<String> dict = new ArrayList<String>();
+      
+      
+      try(Scanner sc = new Scanner(new FileInputStream(wordFile))){
+         
+         while(sc.hasNext()){
+            dict.add(sc.nextLine());
+            wordCount++;
+         }
+         System.out.println("Number of words: " + wordCount);
+      }
+      
+      
+      
+      //randomly assign words from dict to cards[]
+      
+      Random r = new Random();
+      Set<Integer> uniqueNumbers = new HashSet<>();
+      while (uniqueNumbers.size()<25){
+         uniqueNumbers.add(r.nextInt(wordCount));
+      }
+      int counter = 0;
+      for (Integer i : uniqueNumbers)
+      {
+         cards[counter] = new Card(dict.get(i));
+         counter++;
+      }
+      
+     
+      
+      
+      
+      
+      /***
       for(int i = 0; i <25; i++)
       {
-         cards[i] = new Card(inputFile.nextLine());
+         cards[i] = new Card(dict.get(i));
       
       }
-     
+     ***/
    
       
    }
